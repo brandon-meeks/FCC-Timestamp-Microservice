2a5437ac87bcb6633742e27ae17da6f147a552d3
Brandon Meeks <meeksb86@gmail.com>
2024-07-29 19:08:28 +0000

    Completed task

--- index.js
+++ index.js
@@ -20,8 +20,25 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function(req, res) {
+  let pDate = req.params.date;
+  let dateObj;
+  if (!pDate) {
+    dateObj = new Date();
+  }
+  else if (/\d{5,}/.test(pDate)) {
+    console.log(pDate);
+    dateObj = new Date(parseInt(pDate));
+    console.log(dateObj);
+  }
+  else {
+    dateObj = new Date(pDate);
+  }
+  if (isNaN(dateObj)) {
+    res.json({error: "Invalid Date"});
+  } else {
+    res.json({unix: dateObj.getTime(), utc: dateObj.toUTCString()});
+  }
 });
 
 
